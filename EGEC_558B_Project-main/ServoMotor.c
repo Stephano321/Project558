1037c764ea95ad01913ce6e0bf10b902bae04b5d
Stephano321 <85306388+Stephano321@users.noreply.github.com>
2024-12-04 07:42:52 -0800

    Removed PWM Method

--- EGEC_558B_Project-main/ServoMotor.c
+++ EGEC_558B_Project-main/ServoMotor.c
@@ -10,6 +10,7 @@ void Servo_Motor_Init(void)
     GPIO_PORTC_DIR_R |= 0x10;    //Set PC4 as output
     GPIO_PORTC_DEN_R |= 0x10;    //Enable Digital for PC4
 
+    /*
     //PWM Init
     GPIO_PORTC_AFSEL_R |= 0x10;
     GPIO_PORTC_PCTL_R |= 0x00040000;
@@ -27,77 +28,79 @@ void Servo_Motor_Init(void)
     PWM0_3_LOAD_R = 5000 - 1;
 
     //Set Default Comparator Value for the middle position
-    PWM0_3_CMPA_R = (5000 - 350) - 1;
+    PWM0_3_CMPA_R = (5000 - 231) - 1;
     PWM0_CTL_R |= 0x08;
     PWM0_3_CTL_R |= 0x01;
 
     //Enable M0PWMB output
     PWM0_ENABLE_R |= 0x40;
+    */
+
 }
 
 
 void leftPosition(void)
 {
-    PWM0_3_CMPA_R = (5000 - 552) - 1;
-    // Theoretical Value for other Servo
-    //PWM0_3_CMPA_R = (5000-434) - 1;
-
-    /*int i = 0;
+    int i = 0;
     for(i = 0; i < 100; i++)
     {
     GPIO_PORTC_DATA_R |= 0x10;
-    Timer1_Delay_MicroSecond(1735);
+    Timer1_Delay_MicroSecond(1575);
     GPIO_PORTC_DATA_R &= ~0x10;
-    Timer1_Delay_MicroSecond(18265); //Finish Period of 20ms
-    }*/
+    Timer1_Delay_MicroSecond(18425); //Finish Period of 20ms
+    }
+
+    //PWM Method isn't working
+    //PWM0_3_CMPA_R = (5000 - 552) - 1;
+    // Theoretical Value for other Servo
+    //PWM0_3_CMPA_R = (5000-434) - 1;
 }
 
 
 void middlePosition(void)
 {
-    PWM0_3_CMPA_R = (5000-350) - 1;
-    // Theoretical Value for other Servo
-    //PWM0_3_CMPA_R = (5000-231) - 1;
-
-    /*
     int i = 0;
     for(i = 0; i < 100; i++)
     {
     GPIO_PORTC_DATA_R |= 0x10;
-    Timer1_Delay_MicroSecond(925);
+    Timer1_Delay_MicroSecond(920);
     GPIO_PORTC_DATA_R &= ~0x10;
-    Timer1_Delay_MicroSecond(19075); //Finish Period of 20ms
+    Timer1_Delay_MicroSecond(19080); //Finish Period of 20ms
     }
-    */
+
+
+    //PWM Method isn't working
+    //PWM0_3_CMPA_R = (5000-350) - 1;
+    // Theoretical Value for other Servo
+    //PWM0_3_CMPA_R = (5000-231) - 1;
 }
 
 
 void rightPosition(void)
 {
-    PWM0_3_CMPA_R = (5000 - 147) - 1;
-    // Theoretical Value for other Servo
-    //PWM0_3_CMPA_R = (5000-65) - 1;
-
-    /*
     int i = 0;
     for(i = 0; i < 100; i++)
     {
     GPIO_PORTC_DATA_R |= 0x10;
-    Timer1_Delay_MicroSecond(260);
+    Timer1_Delay_MicroSecond(400);
     GPIO_PORTC_DATA_R &= ~0x10;
-    Timer1_Delay_MicroSecond(19740); //Finish Period of 20ms
+    Timer1_Delay_MicroSecond(19600); //Finish Period of 20ms
     }
-    */
+    //PWM Method isn't working
+    //PWM0_3_CMPA_R = (5000 - 147) - 1;
+    // Theoretical Value for other Servo
+    //PWM0_3_CMPA_R = (5000-65) - 1;
+
 }
 
-void Timer1_Delay_MilliSecond(int ttime)
+void Timer1_Delay_MicroSecond(int ttime)
 {
     int i;
     SYSCTL_RCGCTIMER_R |= 2;        // Enable clock to Timer Block 1
     TIMER1_CTL_R = 0;               // Disable Timer1 before initialization
     TIMER1_CFG_R = 0x04;            // Configure for 16-bit mode
     TIMER1_TAMR_R = 0x02;           // Configure for periodic mode and down-counter
-    TIMER1_TAILR_R = 16000 - 1; // Load TimerA interval value (16000 cycles per millisecond)
+    TIMER1_TAILR_R = 16 - 1; // Load TimerA interval value (16000 cycles per millisecond)
     TIMER1_ICR_R = 0x1;             // Clear the TimerA timeout flag
     TIMER1_CTL_R |= 0x01;           // Enable Timer1A after initialization
 
