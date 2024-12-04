2762cefd56ba23627d89dd7cd21292f5e88dc714
Stephano321 <85306388+Stephano321@users.noreply.github.com>
2024-12-03 16:25:58 -0800

    Made modifications to autonomy of robot

--- EGEC_558B_Project-main/main.c
+++ EGEC_558B_Project-main/main.c
@@ -24,9 +24,58 @@ int main(void)
     DC_Motor_Init();
     Servo_Motor_Init();
 
-//    leftPosition();
-//    middlePosition();
-//    rightPosition();
+    middlePosition();                               //Default middle position of servo motor
+    Timer1_Delay_MilliSecond(5000);                 //Get everything ready for testing
+
+
+
+
+    while(1){
+        if(distance > .3)
+            Forward();                              //Nothing close so go forward
+        else
+        {
+            Stop();
+            rightPosition();                        //Move servo to right position to scan
+            Timer1_Delay_MilliSecond(50);
+            if(distance > .3)
+            {
+                Turn_Right();                       //Right side scanned and there's room to travel
+                Timer1_Delay_MilliSecond(500);      //Helps so car can turn right properly
+                Stop()    ;                         //Stops the turning
+                middlePosition();                   //Centers the servo motor so the sensor is in the middle position again
+                Forward();                          //Start Driving since path is clear after it turned right
+            }
+            else
+            {
+                Stop();
+                leftPosition();                     //Too close to the object on the right, now scanning left
+                Timer1_Delay_MilliSecond(50);
+                if(distance > .3)
+                {
+                    Turn_Left();                    //Left side scanned, there's room to travel
+                    Timer1_Delay_MilliSecond(500);  //Helps so far can turn left properly
+                    Stop();                         //Stops the turning
+                    middlePosition();               //Centers the servo motor so the sensor is in the middle position again
+                    Forward();                      //Start Driving since the path is clear after it turned left
+                }
+                else
+                {
+                    middlePosition();               //No path is clear in front, center the servo motor
+                    Reverse();                      //Reverse to find a different route
+                    Timer1_Delay_MilliSecond(2000); //Drives backwards for about 2 seconds
+                    Stop();
+                    int r = rand() % 20;            //Generate random number from 0-19 to pick which way to turn
+                    if (r >= 10)
+                        Turn_Right();               //Greater than or equal to 10, turn right
+                    else
+                        Turn_Left();                //Less than 10, turn left
+
+                }
+            }
+        }
+    }
+
 
 }
 
